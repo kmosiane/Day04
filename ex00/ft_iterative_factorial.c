6e6ae8045e4d21f23f26141e7ee8353d1b01227f
Kenosi Mosiane <kmosiane@c5r1s8.wethinkcode.co.za>
2020-06-26 10:42:39 +0200

    Iterative factorial

---
+++ ex00/ft_iterative_factorial.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmosiane <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/24 14:11:23 by kmosiane          #+#    #+#             */
+/*   Updated: 2020/06/25 13:51:49 by kmosiane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int ft_iterative_factorial(int nb)
+{
+	long factorial;
+	factorial = 1;
+	int i;
+	while(nb <=)
+	{
+		factorial = factorial *1;
+		i++;
+	}
+	return(factorial);
+}
